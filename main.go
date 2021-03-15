9f55fffb9e4f1b7b46aa6c3bd91ea38046f75c32
TotalP <l777Pui1>
2021-03-15 09:10:42 +0200

    main has been simplified.

--- main.go
+++ main.go
@@ -16,16 +16,22 @@ func main() {
 	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
 
 	var wgDoneAll sync.WaitGroup
-	team := make(map[string]*Microservice)
-	prepareTeam(&wgDoneAll, team)
-	runTeam(team)
+	team := make(map[string]*Microservice, 2)
+	team["google"] = newMicroservice(&wgDoneAll, "www.google.com.ua")
+	team["youtube"] = newMicroservice(&wgDoneAll, "www.youtube.com")
+	//run all team
+	for _, value := range team {
+		runMicroservice(value)
+	}
 
 	time.Sleep(5 * time.Second)
-	interruptProgrammatically("event", team)
+	interruptProgrammatically("youtube", team)
 	//main goroutine is blocked until system event happens
-	log.Println("Signal has been caught:", <-signals)
+	<-signals
 	//sending message into each Done channel
-	shutdownTeam(team)
+	for _, value := range team {
+		value.Shutdown <- true
+	}
 	//blocked until all shutdown actions happen
 	wgDoneAll.Wait()
 	log.Println("Chief Executable completed.")
@@ -33,57 +39,35 @@ func main() {
 
 type Microservice struct {
 	ShutdownWaitGroup *sync.WaitGroup
-	Name              string
 	Args              []string
 	Command           *exec.Cmd
 	Shutdown          chan bool
 }
 
-func (m *Microservice) Init(wgDone *sync.WaitGroup, name string, args ...string) {
-	m.ShutdownWaitGroup = wgDone
-	m.ShutdownWaitGroup.Add(1)
-	m.Name = name
-	m.Args = make([]string, len(args))
-	m.Args = append(m.Args, args...)
-	m.Command = exec.Command(m.Name, args...)
-	m.Command.Stdout = os.Stdout
-	m.Shutdown = make(chan bool, 1)
+func newMicroservice(wgDone *sync.WaitGroup, args ...string) *Microservice {
+	var service Microservice
+	service.ShutdownWaitGroup = wgDone
+	service.ShutdownWaitGroup.Add(1)
+	service.Args = append(service.Args, args...)
+	service.Command = exec.Command("ping", args...)
+	service.Command.Stdout = os.Stdout
+	service.Shutdown = make(chan bool, 1)
+	return &service
 }
 
-func (m *Microservice) Run() {
+func runMicroservice(service *Microservice) {
 	go func() {
-		if err := m.Command.Run(); err != nil {
+		log.Println("Starting:", service.Args)
+		if err := service.Command.Run(); err != nil {
 			log.Fatal(err)
 		}
 	}()
 	go func() {
-		defer m.ShutdownWaitGroup.Done()
-		<-m.Shutdown
+		defer service.ShutdownWaitGroup.Done()
+		<-service.Shutdown
 		//graceful shutdown actions
-		log.Println("Shutting down:", m.Name, m.Args)
+		log.Println("Shutting down:", service.Args)
 	}()
-}
-
-func prepareTeam(wgDone *sync.WaitGroup, team map[string]*Microservice) {
-	var data Microservice
-	data.Init(wgDone, "ping", "google.com.ua")
-	team["data"] = &data
-	var event Microservice
-	event.Init(wgDone, "ping", "www.youtube.com")
-	team["event"] = &event
-}
-
-func runTeam(team map[string]*Microservice) {
-	for _, value := range team {
-		log.Println("Starting:", value.Name, value.Args)
-		value.Run()
-	}
-}
-
-func shutdownTeam(team map[string]*Microservice) {
-	for _, value := range team {
-		value.Shutdown <- true
-	}
 }
 
 func interruptProgrammatically(service string, team map[string]*Microservice) {
